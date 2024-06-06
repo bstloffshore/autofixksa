b5524cfcf4baed27297ec6dfdb686707be4bb30c
Raja kumar <rajakumarpusthela@gmail.com>
2024-06-06 12:28:33 +0530

    implemented the condition
    implemented the condition for display the campaign images between start and end dates

--- inc_slider.php
+++ inc_slider.php
@@ -1,6 +1,5 @@
 <section class="home-slider-one">
-    <div class="silder-right-content">
-        
+    <div class="silder-right-content">      
 <!--
         <div class="slider-contact-info">
             <div class="inner">
@@ -8,14 +7,15 @@
                 <div class="icon-holder">
                     <span class="flaticon-whatsapp-logo"></span>
                 </div>
-					</a>
-                
+					</a>                
             </div>
         </div>        
 -->
     </div>
     <div class="home-slider owl-theme owl-carousel banner-carousel-four" dir="ltr">
-        <?php
+        <!--
+            this is old code
+            <?php
         $sliderres = $db->query("SELECT * FROM sliders WHERE active = 1 ORDER BY so ASC");
         while ($sliderrow = mysqli_fetch_assoc($sliderres)) {
             ?>
@@ -24,14 +24,41 @@
             </div>
         </a>
             <?php }  ?>
-
+        -->
+<?php
+    // Get the current date
+    $currentDate = date('Y-m-d');    
+    // Query to get active sliders within the campaign period
+    $sliderres = $db->query("SELECT * FROM sliders WHERE active = 1 AND '$currentDate' BETWEEN campaign_start_date AND campaign_end_date ORDER BY so ASC");
+    // Check if the query returned any results
+    if (mysqli_num_rows($sliderres) > 0) 
+    {
+        // Display the sliders within the campaign period
+        while ($sliderrow = mysqli_fetch_assoc($sliderres)) 
+        {?>
+            <a href="<?php echo $sliderrow['buttonlinkTo']; ?>" class="slide-link" target="_blank">
+                <div class="slide-item" style="height:700px !important;background-image:url('uploads/sliders/<?php echo $sliderrow['image']; ?>');"></div>
+            </a>
+        <?php 
+        } 
+    } else 
+    {
+        // If no campaign sliders are found, display another set of images
+        // Query to get active sliders outside the campaign period
+        $defaultSliderres = $db->query("SELECT * FROM sliders WHERE active = 1 AND ('$currentDate' < campaign_start_date OR '$currentDate' > campaign_end_date)
+        ORDER BY so ASC");
+        while ($defaultSliderrow = mysqli_fetch_assoc($defaultSliderres)) 
+        {?>
+            <!-- <a href="<?php echo $defaultSliderrow['buttonlinkTo']; ?>" class="slide-link" target="_blank">
+            <div class="slide-item" style="height:700px !important;background-image:url('uploads/sliders/<?php echo $defaultSliderrow['image']; ?>');">
+            </div>
+            </a> -->
+            <div class="slide-item" style="height:700px !important;background-image:url('uploads/sliders/AUT0FIXKSA-Slider_01.jpg');">
+        <?php 
+        }
+    }
+?>
     </div>
 </section>
-       <script>
-        function goToLandingPage(url){
-                // window.location.href = url;
-                window.open(url, '_blank');
-
-        }
-        </script>
+       
         
